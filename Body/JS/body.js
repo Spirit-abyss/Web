38d8ff6ba11e631d0297ff327fc4f19802fbdd73
Spirit abyss <114949582+Spirit-abyss@users.noreply.github.com>
2023-11-30 16:00:49 +0300

    Add files via upload

---
+++ Body/JS/body.js
@@ -0,0 +1,13 @@
+window.addEventListener('scroll', e =>
+{
+	document.body.style.cssText += `--scrollTop: ${this.scrollY}px`
+})
+
+document.addEventListener('mousemove', e => {
+	Object.assign(document.documentElement, {
+		style: `
+		--move-x: ${(e.clientX - window.innerWidth / 2) * -.01}deg;
+		--move-y: ${(e.clientY - window.innerHeight / 2) * -.02}deg;
+		`
+	})
+})
